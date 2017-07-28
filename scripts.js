c50bdd7db0fec94aa22f7eb6e54876ae4ca1d51c
kheifer <11204675+kheifer@users.noreply.github.com>
2017-07-28 13:07:02 -0700

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,7 @@
+//uses a math.random to pick the color and .toString(16) to convert to a hexadecimal string
+function randomColorNum(){
+  return (Math.floor(Math.random() * 207) + 48).toString(16);
+}
+function colorGenerator{
+  return '#' + randomColorNum() + randomColorNum() + randomColorNum();
+}
